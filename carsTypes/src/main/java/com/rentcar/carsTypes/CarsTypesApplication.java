2d118c9d228d23cedade365934a6f417b2b710c6
Vladi <mihalasv@gmail.com>
2023-12-13 13:44:09 +0100

    new Spring Boot project

---
+++ carsTypes/src/main/java/com/rentcar/carsTypes/CarsTypesApplication.java
@@ -0,0 +1,13 @@
+package com.rentcar.carsTypes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsTypesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsTypesApplication.class, args);
+	}
+
+}
---
+++ carsTypes/src/test/java/com/rentcar/carsTypes/CarsTypesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentcar.carsTypes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsTypesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
