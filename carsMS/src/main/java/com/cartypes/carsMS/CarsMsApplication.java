73a43b7ea26f601bdc8fd1e741541b03b91d20c2
Vladi <mihalasv@gmail.com>
2023-12-13 12:47:44 +0100

    springboot default project

---
+++ carsMS/src/main/java/com/cartypes/carsMS/CarsMsApplication.java
@@ -0,0 +1,13 @@
+package com.cartypes.carsMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsMsApplication.class, args);
+	}
+
+}
